89c3b6c8c0b3f3f5f99f0377e9157d5f8b37caca
Rainer Martínez Fraga <rainermf@polymitasoft.com>
2017-11-10 22:23:29 -0500

    Make Call to action responsive

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,33 +1,33 @@
 import Section from "./Section";
-import {Button} from "reactstrap";
+import {Button, Row, Col} from "reactstrap";
 import * as React from "react";
 
 export default function CallToAction(props) {
     return (
         <Section dark={props.dark} title={props.title} subtitle={props.subtitle}>
-            <p style={buttonGroupStyle}>
-                <ActionButton outline text={props.option1} />
-                <Separator/>
-                <ActionButton text={props.option2}/>
-            </p>
+            <Row style={buttonGroupStyle}>
+                <Col md={6} sm={12} xs={12} className={"mb-2 text-xs-center text-sm-center text-md-right"}>
+                    <ActionButton left outline text={props.option1}/>
+                </Col>
+                <Col md={6} sm={12} xs={12} className={"mb-2 text-xs-center text-sm-center text-md-left"}>
+                    <ActionButton text={props.option2}/>
+                </Col>
+            </Row>
         </Section>
     );
 }
 
-function Separator() {
-    return <span style={{marginRight: "4rem"}}/>
-}
-
 function ActionButton(props) {
+    const margin = (props.left) ? "mr-md-5 mr-sm-0" : "ml-md-5 ml-sm-0";
+
     return (
-        <Button style={buttonStyle} outline={props.outline} color="primary">
+        <Button className={margin} style={buttonStyle} outline={props.outline} color="primary">
             {props.text}
         </Button>
     );
 }
 
 const buttonGroupStyle = {
-    textAlign: "center",
     paddingTop: "1rem",
 };
 
