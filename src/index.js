58abc881483a23c41bf88964c472586cc1788cbe
Rainer Martínez Fraga <rainermf@polymitasoft.com>
2017-11-16 00:01:41 -0500

    Implement scroll to top in routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./components/App"
 import { BrowserRouter } from "react-router-dom"
+import ScrollToTop from "./components/ScrollToTop"
 import LanguageProvider from "./components/LanguageProvider"
 import { translationMessages } from "./i18n"
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker'
 import "simple-line-icons/css/simple-line-icons.css"
-import "./bootstrap.css";
+import "./bootstrap.css"
 import "typeface-roboto"
 
 // ========================================
@@ -14,10 +15,12 @@ import "typeface-roboto"
 ReactDOM.render((
   <LanguageProvider locale="it" messages={translationMessages}>
     <BrowserRouter>
-      <App />
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
     </BrowserRouter>
   </LanguageProvider>
   ), document.getElementById('root')
-);
+)
 
-registerServiceWorker();
+registerServiceWorker()
