1aae7d9755ae8895444b3849f062f28a2d655d6f
Rainer Martínez Fraga <rmartinezfraga@yandex.com>
2017-11-22 10:53:02 -0500

    Mejorar contenido de página acerca de...

--- src/components/About/messages.js
+++ src/components/About/messages.js
@@ -1,8 +1,3 @@
-/*
- * HomePage Messages
- *
- * This contains all the text for the HomePage component.
- */
 import {defineMessages} from 'react-intl';
 
 export default defineMessages({
@@ -13,42 +8,3 @@ export default defineMessages({
         id: 'cc.About.intro',
     }
 });
-
-export const services = [
-    "Consulenze per richieste di sanatoria, permesso di soggiorno, carta di soggiorno, assistenza di  servizi accessori, amministrativi e burocratici;",
-    "Richiesta di visto per turismo o ricongiungimento familiare, polizze assicurative, assicurazioni sanitarie;",
-    "Presentazione per omologazioni di titoli di studio, o della cittadinanza italiana;",
-    "Traduzioni libere o giurate di certificati, diplomi e documenti;",
-    "Richiesta e legalizzazione di certificati e documenti presso organismi, istituzioni pubbliche o private italiane;",
-    "Organizzazione di corsi in lingua Italiana o straniera, manifestazioni culturali, corsi di musica, balli e danze tradizionali;",
-    "Richiesta per conto dell’ associato nel proprio paese d’ origine di documenti, certificati, traduzioni, legalizzazioni, matrimoni e divorzi per procura, trascrizione atti;",
-    "Emissione e prenotazioni di biglietti aerei, ferroviari, marittimi, di trasporti su strada e vari, pacchetti turistici, noleggio auto, prenotazione alberghi/ case private,",
-    "Spedizione e consegna pacchi postali nel paese di residenza dello straniero per via aerea e marittima;",
-    "Rilascio visti ed assicurazione di viaggio per Cuba;",
-    "Istituzione, gestione e rappresentanza di agenzie di viaggio e turismo di qualsiasi tipo e categoria, italiane e straniere, in Italia e all’estero;",
-    "La promozione ed organizzazione del turismo individuale e collettivo, per conto proprio e per conto terzi;",
-    "Rappresentanza, organizzazione, pianificazione con qualsiasi forma e mezzo di programmi, progetti ed iniziative di viaggio siano queste destinate al turismo e/o a qualsiasi altro settore;",
-    "Rappresentanza, produzione e promozione di guide, libri e cataloghi turistici, villaggi turistici, agriturismo, bed & breakfast, campeggi, bar, ristoranti sia in Italia che all’ estero;",
-    "Raccolta e spedizione di pacchi per aiuto socio , \"umanitario a Cuba;",
-    "Pratiche su delega dell’ associato presso qualsiasi consolato straniero in Italia e al estero",
-    "Organizzazione mostre d’ arte o altre attività che permettano la diffusione di costumi, tradizioni, religioni, arti culinarie, prodotti e bevande alcoliche ed analcoliche tipici di altri paesi;",
-    "Promuovere iniziative di solidarietà a vantaggio dei residenti stranieri., \"consulenze amministrative e legali a cittadini stranieri sul territorio italiano e per i cittadini italiani residenti al estero riguardo all’ attuale  paese di residenza;",
-    "Consulenze per richieste di sanatoria, permesso di soggiorno, carta di soggiorno, assistenza di  servizi accessori, amministrativi e burocratici;",
-    "Richiesta di visto per turismo o ricongiungimento familiare, polizze assicurative, assicurazioni sanitarie;",
-    "Presentazione per omologazioni di titoli di studio, o della cittadinanza italiana;",
-    "Traduzioni libere o giurate di certificati, diplomi e documenti;",
-    "Richiesta e legalizzazione di certificati e documenti presso organismi, istituzioni pubbliche o private italiane;",
-    "Organizzazione di corsi in lingua Italiana o straniera, manifestazioni culturali, corsi di musica, balli e danze tradizionali;",
-    "Richiesta per conto dell’ associato nel proprio paese d’ origine di documenti, certificati, traduzioni, legalizzazioni, matrimoni e divorzi per procura, trascrizione atti;",
-    "Emissione e prenotazioni di biglietti aerei, ferroviari, marittimi, di trasporti su strada e vari, pacchetti turistici, noleggio auto, prenotazione alberghi/ case private,",
-    "Spedizione e consegna pacchi postali nel paese di residenza dello straniero per via aerea e marittima;",
-    "Rilascio visti ed assicurazione di viaggio per Cuba;",
-    "Istituzione, gestione e rappresentanza di agenzie di viaggio e turismo di qualsiasi tipo e categoria, italiane e straniere, in Italia e all’estero;",
-    "La promozione ed organizzazione del turismo individuale e collettivo, per conto proprio e per conto terzi;",
-    "Rappresentanza, organizzazione, pianificazione con qualsiasi forma e mezzo di programmi, progetti ed iniziative di viaggio siano queste destinate al turismo e/o a qualsiasi altro settore;",
-    "Rappresentanza, produzione e promozione di guide, libri e cataloghi turistici, villaggi turistici, agriturismo, bed & breakfast, campeggi, bar, ristoranti sia in Italia che all’ estero;",
-    "Raccolta e spedizione di pacchi per aiuto socio , \"umanitario a Cuba;",
-    "Pratiche su delega dell’ associato presso qualsiasi consolato straniero in Italia e al estero",
-    "Organizzazione mostre d’ arte o altre attività che permettano la diffusione di costumi, tradizioni, religioni, arti culinarie, prodotti e bevande alcoliche ed analcoliche tipici di altri paesi;",
-    "Promuovere iniziative di solidarietà a vantaggio dei residenti stranieri."
-];
