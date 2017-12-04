9a2813869fca128cfd98053040b52114c15fe89a
Rainer Martínez Fraga <rmartinezfraga@yandex.com>
2017-12-03 23:49:38 -0500

    Add support for responsive images

--- src/components/Home/Slider.js
+++ src/components/Home/Slider.js
@@ -2,6 +2,10 @@ import trinidadImage from "img/trinidad.jpg"
 import puenteImage from "img/puente.jpg"
 import carroImage from "img/carro.jpg"
 import playaImage from "img/playa.jpg"
+import trinidadImageSmall from "img/trinidad-small.jpg"
+import puenteImageSmall from "img/puente-small.jpg"
+import carroImageSmall from "img/carro-small.jpg"
+import playaImageSmall from "img/playa-small.jpg"
 import Section from "../Section"
 import Col from "reactstrap/lib/Col"
 import Row from "reactstrap/lib/Row"
@@ -16,16 +20,16 @@ export default class Slider extends React.Component {
             <Section title={<FormattedMessage {...messages.sliderTitle} />}>
                 <SliderContainer>
                     <ImgContainer>
-                        <Img src={trinidadImage} alt="Trinidad" />
+                        <ResponsiveImage small={trinidadImageSmall} large={trinidadImage} alt="Trinidad" />
                     </ImgContainer>
                     <ImgContainer>
-                        <Img src={puenteImage} alt="Puente" />
+                        <ResponsiveImage small={puenteImageSmall} large={puenteImage} alt="Puente" />
                     </ImgContainer>
                     <ImgContainer>
-                        <Img src={carroImage} alt="Almendrón" />
+                        <ResponsiveImage small={carroImageSmall} large={carroImage} alt="Almendrón" />
                     </ImgContainer>
                     <ImgContainer>
-                        <Img src={playaImage} alt="Varadero" />
+                        <ResponsiveImage large={playaImage} small={playaImageSmall} alt="Varadero" />
                     </ImgContainer>
                 </SliderContainer>    
             </Section>    
@@ -47,3 +51,9 @@ const Img = styled.img`
     width: 100%;
     box-shadow: 0 5px 4px -4px gray;
 `
+
+const ResponsiveImage = (props) => <Img src={props.small}
+        srcSet={`${props.large} 500w, ${props.small} 255w`}
+        sizes="(min-width: 768px) 25.0vw, 100vw"
+        alt={props.alt}
+    />
